a5307c04d2bb3affd404606e59600d78962464ea
MortenEvensen <evensenmorten@hotmail.com>
2020-06-22 00:27:14 +0200

    minor updates

---
+++ src/components/navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Navbar() {
+	return(
+		<>
+		<div className="navbar">
+			<div className="logo-wrap">
+				<h1>Holidaze</h1>
+			</div>
+			<div className="navigation-wrap">
+				<ul>
+					<li>Hotels</li>
+					<li>Contact</li>
+					<li>Booking</li>
+				</ul>
+			</div>
+		</div>
+		</>
+	)
+}
+
+export default Navbar
